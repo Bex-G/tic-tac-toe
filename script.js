30fdce04a8281643d15cb6b848c6ec83ac8e8f24
Bex-G <bexgiacinto@gmail.com>
2023-08-13 19:41:53 -0700

    Attach click event to gameBoard properties for scorekeeping.

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const gameBoard = (() => {
-    let game = {
-        a:["", "", ""],
-        b:["", "", ""],
-        c:["", "", ""],
+const game = (() => {
+    let gameBoard = {
+        'a1': "",
+        'a2': "",
+        'a3': "",
+        'b1': "",
+        'b2': "",
+        'b3': "",
+        'c1': "",
+        'c2': "",
+        'c3': "",
     }
-    return game;
+    function addToBoard(target) {
+        gameBoard[target] = token;
+    }
+    return {gameBoard, addToBoard}
 })();
 
 const playerFactory = (name, token) => {
@@ -13,3 +22,20 @@ const playerFactory = (name, token) => {
 
 const bex = playerFactory("Bex", "X");
 const carly = playerFactory("Carly", "O");
+
+let token = "X";
+
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach(box => {
+    box.addEventListener('click', function fillBox(e) {
+
+    const boxContent = document.createTextNode(token)
+
+    let target = e.target.id;
+
+    game.addToBoard(target);
+
+    box.appendChild(boxContent);
+    }, {once : true});
+});
