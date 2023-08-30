359ee9fea91c751068a796c891634fed0ab5b598
Bex-G <bexgiacinto@gmail.com>
2023-08-30 13:51:05 -0700

    Clean up existing code by removing extraneous language.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const playerFactory = (name, token, score) => {
-    return {name, token, score};
-}
+const playerFactory = (name, token) => {
+    return {name, token};
+};
 
-const playerX = playerFactory("X", "X", 0);
-const playerO = playerFactory("O", "O", 0);
+const playerX = playerFactory("X", "X");
+const playerO = playerFactory("O", "O");
 
 const game = (() => {
     let gameBoard = {
@@ -17,82 +17,64 @@ const game = (() => {
         'c2': "8",
         'c3': "9",
     }   
-    function addToBoard(target) {
+    function addToBoard(target) { //modifies gameBoard object for running equality checks
         gameBoard[target] = token;
     }
-    function takeTurns(turnNumber) {
+    function takeTurns(turnNumber) { //changes display and switches token value with each turn
         if (turnNumber % 2 === 0) {
+            display.textContent = playerO.name + "'s turn"; 
             return token = "X";
         } else {
+            display.textContent = playerX.name + "'s turn";
             return token = "O";
         }
     }
-    function changeTurnDisplay(turnNumber) {
-            if (turnNumber % 2 === 0) { 
-                display.textContent = playerO.name + "'s turn";      
-            } else {
-                display.textContent = playerX.name + "'s turn";
-            }
-    }
     function callTheRound() {
-        function findWinner(winningToken) {
-            if (winningToken === playerX.token) {
+        function declareWinner() {
+            if (token === playerX.token) {
                 display.textContent = playerX.name + " wins!";
             } else {
                 display.textContent = playerO.name + " wins!";
             }
         }
         if (gameBoard.a1 === gameBoard.a2 && gameBoard.a2 === gameBoard.a3) {
-            let winningToken = gameBoard.a1;
-            findWinner(winningToken);
+            declareWinner();
         } else if (gameBoard.b1 === gameBoard.b2 && gameBoard.b2 === gameBoard.b3) {
-            let winningToken = gameBoard.b1;
-            findWinner(winningToken);
+            declareWinner();
         } else if (gameBoard.c1 === gameBoard.c2 && gameBoard.c2 === gameBoard.c3) {
-            let winningToken = gameBoard.c1;
-            findWinner(winningToken);
+            declareWinner();
         } else if (gameBoard.a1 === gameBoard.b1 && gameBoard.b1 === gameBoard.c1) {
-            let winningToken = gameBoard.a1;
-            findWinner(winningToken);
+            declareWinner();
         } else if (gameBoard.a2 === gameBoard.b2 && gameBoard.b2 === gameBoard.c2) {
-            let winningToken = gameBoard.a2;
-            findWinner(winningToken);
+            declareWinner();
         } else if (gameBoard.a3 === gameBoard.b3 && gameBoard.b3 === gameBoard.c3) {
-            let winningToken = gameBoard.a3;
-            findWinner(winningToken);
+            declareWinner();
         } else if (gameBoard.a1 === gameBoard.b2 && gameBoard.b2 === gameBoard.c3) {
-            let winningToken = gameBoard.a1;
-            findWinner(winningToken);
+            declareWinner();
         } else if (gameBoard.a3 === gameBoard.b2 && gameBoard.b2 === gameBoard.c1) {
-            let winningToken = gameBoard.a3;
-            findWinner(winningToken);
+            declareWinner();
         } else if (turnNumber === 10) {
             display.textContent = "it's a tie";
         }
     }
-    return {gameBoard, takeTurns, changeTurnDisplay, addToBoard, callTheRound};
+    return {gameBoard, takeTurns, addToBoard, callTheRound};
 })();
 
-const display = document.getElementById('display');
-display.textContent = playerX.name + "'s turn";
+document.getElementById('display').textContent = playerX.name + "'s turn"; //dynamic display
 
-let turnNumber = 1;
+let turnNumber = 1; //global starting value for event-triggered counter
 
-const boxes = document.querySelectorAll('.box');
-
-boxes.forEach(box => {
+document.querySelectorAll('.box').forEach(box => {
     box.addEventListener('click', function fillBox(e) {
 
     ++turnNumber;
-    
+
     game.takeTurns(turnNumber);
-    game.changeTurnDisplay(turnNumber);
 
     const boxContent = document.createTextNode(token);
     box.appendChild(boxContent);
     
     let target = e.target.id;
-
     game.addToBoard(target);
     game.callTheRound();
     }, {once : true});
