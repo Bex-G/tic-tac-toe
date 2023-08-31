2048fdd83c3189cd31307a5c81f28dc160647baf
Bex-G <bexgiacinto@gmail.com>
2023-08-31 14:43:42 -0700

    Create button and add functions to reset gameBoard and displays.

--- script.js
+++ script.js
@@ -32,50 +32,99 @@ const game = (() => {
     function callTheRound() {
         function declareWinner() {
             if (token === playerX.token) {
-                display.textContent = playerX.name + " wins!";
+                display.textContent = playerX.name + " won the game!";
+                return gameover = true;
             } else {
-                display.textContent = playerO.name + " wins!";
+                display.textContent = playerO.name + " won the game!";
+                return gameover = true;
             }
         }
         if (gameBoard.a1 === gameBoard.a2 && gameBoard.a2 === gameBoard.a3) {
             declareWinner();
+            revertBoard();
         } else if (gameBoard.b1 === gameBoard.b2 && gameBoard.b2 === gameBoard.b3) {
             declareWinner();
+            revertBoard();
         } else if (gameBoard.c1 === gameBoard.c2 && gameBoard.c2 === gameBoard.c3) {
             declareWinner();
+            revertBoard();
         } else if (gameBoard.a1 === gameBoard.b1 && gameBoard.b1 === gameBoard.c1) {
             declareWinner();
+            revertBoard();
         } else if (gameBoard.a2 === gameBoard.b2 && gameBoard.b2 === gameBoard.c2) {
             declareWinner();
+            revertBoard();
         } else if (gameBoard.a3 === gameBoard.b3 && gameBoard.b3 === gameBoard.c3) {
             declareWinner();
+            revertBoard();
         } else if (gameBoard.a1 === gameBoard.b2 && gameBoard.b2 === gameBoard.c3) {
             declareWinner();
+            revertBoard();
         } else if (gameBoard.a3 === gameBoard.b2 && gameBoard.b2 === gameBoard.c1) {
             declareWinner();
+            revertBoard();
         } else if (turnNumber === 10) {
-            display.textContent = "it's a tie";
+            display.textContent = "It's a tie - try again!";
+            revertBoard();
+            return gameover = true;
         }
     }
-    return {gameBoard, takeTurns, addToBoard, callTheRound};
+    function revertBoard() {
+        return gameBoard = {
+            'a1': "1",
+            'a2': "2",
+            'a3': "3",
+            'b1': "4",
+            'b2': "5",
+            'b3': "6",
+            'c1': "7",
+            'c2': "8",
+            'c3': "9",
+        }
+    }
+    function reset() {
+        gameover = false;
+        turnNumber = 1;
+        document.getElementById('display').textContent = playerX.name + "'s turn";
+        revertBoard();
+        document.getElementById('a1').textContent = "";
+        document.getElementById('a2').textContent = "";
+        document.getElementById('a3').textContent = "";
+        document.getElementById('b1').textContent = "";
+        document.getElementById('b2').textContent = "";
+        document.getElementById('b3').textContent = "";
+        document.getElementById('c1').textContent = "";
+        document.getElementById('c2').textContent = "";
+        document.getElementById('c3').textContent = "";
+    }
+    return {gameBoard, takeTurns, addToBoard, callTheRound, reset};
 })();
 
-document.getElementById('display').textContent = playerX.name + "'s turn"; //dynamic display
-
+let gameover = false;
 let turnNumber = 1; //global starting value for event-triggered counter
+document.getElementById('display').textContent = playerX.name + "'s turn"; //dynamic display
 
 document.querySelectorAll('.box').forEach(box => {
     box.addEventListener('click', function fillBox(e) {
 
-    ++turnNumber;
-
-    game.takeTurns(turnNumber);
-
-    const boxContent = document.createTextNode(token);
-    box.appendChild(boxContent);
+        if (box.textContent !== "X" && box.textContent !== "O" && gameover !== true) {
     
-    let target = e.target.id;
-    game.addToBoard(target);
-    game.callTheRound();
-    }, {once : true});
-})
+            ++turnNumber;
+            game.takeTurns(turnNumber);
+
+            box.textContent = token;
+
+            let target = e.target.id;
+
+            game.addToBoard(target);
+            game.callTheRound();
+        } else if (gameover === true) {
+            game.reset();
+        }
+    })
+});
+
+const resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', () => {
+    game.reset();
+});
